c752641f4f265988d722340b3646ec3225c9ee69
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-19 20:55:18 -0800

    Our conditionals!

---
+++ conditionals.py
@@ -0,0 +1,67 @@
+# if "Sammie" > "Leah":
+#     print "My name is greater!"
+# elif "Leah" > "Sammie":
+#     print "Their name is greater."
+# else:
+#     print "Our names are equal!"
+
+# date = 19
+# if date > 15:
+#     print "Oh, we're halfway there!"
+# else:
+#     print "The month is still young."
+
+# today = "Thursday"
+# if today == "Monday":
+#     print "Yaaas Monday! Here we go!"
+# elif today == "Tuesday":
+#     print "Sigh, it's only Tuesday."
+# elif today == "Wednesday":
+#     print "Humpday!"
+# elif today == "Thursday":
+#     print "#tbt"
+# elif today == "Friday":
+#     print "TGIF!"
+# else:
+#     print "Yeah, it's the weekend!"
+
+# age = 33
+# # if age >= 18:
+# #     print "Yay! I can vote!"
+# # else:
+# #     print "Aww, I cannot vote."
+
+# # if age >= 18 and age >= 21:
+# #     print "I can vote and go to a bar!"
+# if age >= 21:
+#     print "I can go to a bar."
+# elif age >= 18:
+#     print "I can vote, but I cannot go to a bar."
+# else:
+#     print "I cannot vote or go to a bar."
+
+# if 8 % 2 == 0:
+#     print "The number 8 is even."
+# else: 
+#     print "The number 8 is odd."
+
+# if 8 % 2 != 0:
+#     print "The number 8 is odd."
+# else:
+#     print "The number 8 is even."
+
+# if 8 % 2 == 0 and 9 % 2 == 0:
+#     print "Both numbers are even."
+# elif 8 % 2 == 0 and 9 % 2 != 0:
+#     print "8 is even and 9 is odd."
+# elif 8 % 2 != 0 and 9 % 2 == 0:
+#     print "8 is odd and 9 is even."
+# else:
+#     print "Both numbers are odd."
+
+favorite_color = "black"
+if favorite_color == "blue" or favorite_color == "yellow" or favorite_color == "red":
+    print "My favorite color is primary color."
+else: 
+    print "My favorite color is a secondary color."
+
